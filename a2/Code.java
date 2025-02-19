0c6a63391c6c943182b69e76a5e3fc516f268962
Alex Bass <alexbass@csus.edu>
2025-02-19 12:44:10 -0800

    blerp

---
+++ a2/Code.java
@@ -0,0 +1,112 @@
+package a2;
+
+import javax.swing.JFrame;
+
+import static com.jogamp.opengl.GL4.*;
+
+import java.nio.FloatBuffer;
+import java.lang.Math;
+
+import com.jogamp.common.nio.Buffers;
+import com.jogamp.opengl.GL4;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLContext;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.awt.GLCanvas;
+
+import org.joml.*;
+
+public class Code extends JFrame implements GLEventListener {
+    private GLCanvas myCanvas;
+    private int renderingProgram;
+    private int vao[] = new int[1];
+    private int vbo[] = new int[2];
+    private float cameraX, cameraY, cameraZ;
+    private float cubeLocX, cubeLocY, cubeLocZ;
+
+
+
+    // display function variables
+    private FloatBuffer vals = Buffers.newDirectFloatBuffer(16);
+    private Matrix4f pMat = new Matrix4f();
+    private Matrix4f vMat = new Matrix4f();
+    private Matrix4f mMat = new Matrix4f();
+    private Matrix4f mvMat = new Matrix4f();
+    private int mvLoc, pLoc;
+    private float aspect;
+
+    public Code() {
+        setTitle("CSC155 - Assignment 2");
+        setSize(600, 600);
+        setLocation(200, 200);
+        myCanvas = new GLCanvas();
+        myCanvas.addGLEventListener(this);
+        this.add(myCanvas);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+        GL4 gl = (GL4) GLContext.getCurrentGL();
+        gl.glClear(GL_COLOR_BUFFER_BIT);
+        gl.glClear(GL_DEPTH_BUFFER_BIT);
+
+        gl.glUseProgram(renderingProgram);
+
+        mvLoc = gl.glGetUniformLocation(renderingProgram, "mv_matrix");
+        pLoc = gl.glGetUniformLocation(renderingProgram, "p_matrix");
+
+        aspect = (float) myCanvas.getWidth() / (float) myCanvas.getHeight();
+        pMat.setPerspective((float) Math.toRadians(60.0f), aspect, 0.1f, 1000.0f);
+
+        vMat.translation(-cameraX, -cameraY, -cameraZ);
+
+        // draw cube
+
+        mMat.translation(cubeLocX, cubeLocY, cubeLocZ);
+
+        mvMat.identity().mul(vMat).mul(mMat);
+
+        gl.glUniformMatrix4fv(mvLoc, 1, false, mvMat.get(vals));
+        gl.glUniformMatrix4fv(pLoc, 1, false, pMat.get(vals));
+
+        gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+        gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        gl.glEnableVertexAttribArray(0);
+
+        gl.glEnable(GL_DEPTH_TEST);
+        gl.glDepthFunc(GL_LEQUAL);
+
+        gl.glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+
+    @Override
+    public void init(GLAutoDrawable drawable) {
+        GL4 gl = (GL4) GLContext.getCurrentGL();
+        renderingProgram = Utils.createShaderProgram("a2/vertShader.glsl", "a2/fragShader.glsl");
+        setupVertices();
+        cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
+        cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+    }
+
+    private void setupVertices() {
+        GL4 gl = (GL4) GLContext.getCurrentGL();
+        Cube cube = new Cube();
+
+        gl.glGenVertexArrays(vao.length, vao, 0);
+        gl.glBindVertexArray(vao[0]);
+        gl.glGenBuffers(vbo.length, vbo, 0);
+
+        gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+        FloatBuffer cubeBuf = Buffers.newDirectFloatBuffer(cube.getVertices());
+        gl.glBufferData(GL_ARRAY_BUFFER, cubeBuf.limit() * 4, cubeBuf, GL_STATIC_DRAW);
+
+    }
+
+    public static void main(String[] args) { new Code(); }
+    @Override
+    public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {}
+    @Override
+    public void dispose(GLAutoDrawable arg0) {}
+
+}
